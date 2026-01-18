a05cdcce4144c1029661734fbe019c0a6dfe72e6
mazafrav <12997270+mazafrav@users.noreply.github.com>
2026-01-18 12:43:01 +0100

    Created main sections and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+
+import { Navbar } from "@/layout/Navbar"
+import { Hero } from "@/sections/Hero"
+import { About } from "@/sections/About"
+import { Experience } from "@/sections/Experience"
+import { Projects } from "@/sections/Projects"
+
+
 function App() {
-  return <></>;
+  return <div className="min-h-screen overflow-x-hidden">
+    <Navbar />
+    <main>
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+    </main>
+  </div>;
 }
 
 export default App;
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return <header>
+        <nav>
+            <a>
+                MZ<span>.</span>
+            </a>
+        </nav>
+    </header>
+};
