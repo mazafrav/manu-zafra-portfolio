68cef5da7ab76845eb7f72209607704ea0accd14
mazafrav <12997270+mazafrav@users.noreply.github.com>
2026-01-18 18:02:12 +0100

    Navbar update, Added button component

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,5 @@
+import {Button} from "@/components/Button"
+
 const navLinks = [
     {href: "#about", label: "About"},
     {href: "#experience", label: "Experience"},
@@ -8,20 +10,26 @@ export const Navbar = () => {
     return (
     <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
         <nav className="container mx-auto px-6 flex items-center justify-between">
-            <a href="#" className="text-xl font-bold tracking-tight">
-                MZ<span>.</span>
+            <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
+                MZ<span className="text-primary">.</span>
             </a>
 
             {/* Desktop Nav*/}
-            <div>
-                <div>
+            <div className="flex items-center gap-1">
+                <div className="glass rounded-full px-2 py-1 flex item-center gap-1">
                     {navLinks.map((link, index) => (
-                        <a href={link.href} key={index}>
+                        <a href={link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foregound rounded-full hover:bg-surface">
                             {link.label}
                         </a>
                     ))}
                 </div>
             </div>
+
+            {/*CTA Button*/}
+            <div>
+                <Button size="sm">Contact me</Button>
+            </div>
+
         </nav>
     </header>
     )
