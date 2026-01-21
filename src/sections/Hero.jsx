bee5036426379ca95694aa49295d498e1865691c
mazafrav <12997270+mazafrav@users.noreply.github.com>
2026-01-21 09:01:19 +0100

    Added bg image to hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,14 @@
 export const Hero = () => {
-    return <section></section>
+    return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* BG */}
+        <div>
+            <img 
+                src="/blackbg.png" 
+                alt="Portrait" 
+                className="w-full h-full object-cover opacity-40"
+            />
+        </div>
+    </section>
+    )
 };
